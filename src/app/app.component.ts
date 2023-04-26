f4f4430690292f8220155c1524c5272cbd50678c
GitAnthemBatangyagit2006 <Rhadomil.Paras@anthem.com>
2023-04-26 14:40:11 -0400

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { IWindow } from './interfaces/Window';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  users: any;
+  benefitKeywordsFound: any;
   results$: Observable<any>;
   subject = new Subject();
   constructor(private api: ApiService, @Inject('Window') private window: IWindow) {}
@@ -18,9 +18,14 @@ export class AppComponent {
   ngOnInit() {
     this.results$ = this.subject.pipe(
       debounce(() => interval(1000)),
-      map((searchText: string) => this.apiCall(searchText))
     );
+    
+    this.results$.subscribe((searchText)=> {
+      this.apiCall(searchText);
+    })
+    
   }
+
   search(evt) {
     const searchText = evt.target.value;
     // emits the `searchText` into the stream. This will cause the operators in its pipe function (defined in the ngOnInit method) to be run. `debounce` runs and then `map`. If the time interval of 1 sec in debounce hasn't elapsed, map will not be called, thereby saving the server from being called.
@@ -29,8 +34,12 @@ export class AppComponent {
 
   apiCall (searchText: string) {
     this.api.get('users?page=1').subscribe(res => {
-      this.users = res;
-      console.log('data response', this.users);
+      console.log('data response', res.data);
+      this.buildKeywordList(res.data);
     });
+  }
+  
+  buildKeywordList(data: any) {
+    this.benefitKeywordsFound = data.map((d) => d.first_name);
   }
 }
---
+++ src/app/interfaces/benefitsNLP.ts
@@ -0,0 +1,3 @@
+export interface BenefitKeywordsDTO {
+  benefitKeywords: string[];
+}
