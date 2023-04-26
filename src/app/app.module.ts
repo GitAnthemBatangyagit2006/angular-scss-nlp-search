7a15012fe866c88c965007ddcf3f804a7b6a779e
GitAnthemBatangyagit2006 <Rhadomil.Paras@anthem.com>
2023-04-26 13:55:12 -0400

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,24 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NewComponent } from './new/new.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ApiService } from './app.service';
+
+export function getWindow() {
+  return window;
+}
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NewComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, HelloComponent, NewComponent],
+  providers: [
+    HttpClient,
+    ApiService,
+    {
+      provide: 'Window',
+      useFactory: getWindow,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
