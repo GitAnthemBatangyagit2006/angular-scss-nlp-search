138ccfec6a0120830baad7eac3236873c8559aef
GitAnthemBatangyagit2006 <Rhadomil.Paras@anthem.com>
2023-05-18 13:06:04 -0400

    1 file modified, 1 added

---
+++ src/app/nlp/summary/benefitNLPSearchSummaryCmp.ts
@@ -0,0 +1,24 @@
+import { Component, ElementRef, HostListener, Inject, ViewChild, OnInit } from '@angular/core';
+import { Observable, Subject, interval } from 'rxjs';
+import { debounce, map } from 'rxjs/operators';
+import { ApiService } from './../../../app.service';
+import { IWindow } from './../../../interfaces/Window';
+
+@Component({
+  selector: 'search-summary',
+  templateUrl: './benefitNLPSearchSummary.html',
+  styleUrls: ['./new.component.scss'],
+})
+export class BenefitNLPSearchSummaryComponent implements OnInit {
+  
+  constructor(
+    private api: ApiService,
+    @Inject('Window') private window: IWindow,
+    private currentComponent: ElementRef
+  ) {}
+
+  ngOnInit() {
+   
+  }
+
+}
