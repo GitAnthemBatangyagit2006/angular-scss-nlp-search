f21de07918e9db99cc40d39df81a5760d512bc2e
GitAnthemBatangyagit2006 <Rhadomil.Paras@anthem.com>
2023-05-23 12:41:50 -0400

    2 files modified

--- src/app/nlp/summary/benefitNLPSearchSummaryCmp.ts
+++ src/app/nlp/summary/benefitNLPSearchSummaryCmp.ts
@@ -3,6 +3,7 @@ import { Observable, Subject, interval } from 'rxjs';
 import { debounce, map } from 'rxjs/operators';
 import { ApiService } from '../../app.service';
 import { IWindow } from '../../interfaces/Window';
+import { BenefitSummarySearchResult } from './benefitNLP';
 
 @Component({
   selector: 'search-summary',
@@ -10,7 +11,42 @@ import { IWindow } from '../../interfaces/Window';
   styleUrls: ['./benefitNLPSearchSummary.scss'],
 })
 export class BenefitNLPSearchSummaryComponent implements OnInit {
-  
+  benefitNLPSearchSummaryData: BenefitSummarySearchResult | undefined;
+  content = {
+    title: 'Benefit Coverage',
+    searchBar: {
+      noMatchingKeywords: 'No medical service found for searched text.',
+      inputKeywordAriaLabel: 'Enter 3 or more characters to start a search.',
+      inputKeywordHint: 'Search by service or procedure...',
+      showLess: 'Show Less',
+      showMore: 'Show More',
+      defaultCountOfKeywordsShown: 1,
+      serviceTitle: '',
+    },
+    resultText: '2 result for',
+    benefit: {
+      copay: 'Copay',
+      coinsurance: 'Coinsurance',
+      deductibleApplies: 'Deductible Applies',
+      priorAuthorization: 'Prior Authorization',
+      viewDetailAnalyticTag: 'viewDetailAnalyticTag',
+      viewDetailAriaLabel: 'viewDetailAriaLabel',
+      viewDetails: 'View Details',
+      saveforlater: 'Save for later',
+    },
+    filter: {
+      title: 'Filter',
+      clearAll: 'clear All',
+      category: 'category',
+      serviceLocation: 'service Location',
+      network: 'Provider Network',
+      clearAllAnalyticTag: 'clearAllAnalyticTag',
+      clearAllAriaLabel: 'clearAllAriaLabel',
+    },
+    backToBenefits: 'Back To Benefits',
+    backToBenefitsAnalyticTag: 'backToBenefitsAnalyticTag',
+    backToBenefitsAriaLabel: 'backToBenefitsAriaLabel',
+  };
   constructor(
     private api: ApiService,
     @Inject('Window') private window: IWindow,
